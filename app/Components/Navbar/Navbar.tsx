64306baa2d394bd783aa29c4f68c8a7145b6a86b
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2026-04-12 14:44:01 +0100

    completed the mobile menu

--- app/Components/Navbar/Navbar.tsx
+++ app/Components/Navbar/Navbar.tsx
@@ -70,7 +70,7 @@ const Navbar = () => {
             Luxe<span className="text-(--prim)">space</span>
           </Link>
 
-          {/* Desktop Menu */}
+          {/* Desktop Navlink */}
           <nav className="hidden lg:flex space-x-6 menu-link relative ms-10">
             {navLinks.map((link) =>
               link.dropdown ? (
@@ -121,7 +121,43 @@ const Navbar = () => {
           </button>
         </div>
       </div>
-
+      {/* Mobile Navlink */}
+      <div className={`lg:hidden bg-(--white) border-t border-gray-400 overflow-hidden transition-all duration-500 ${mobileMenuOpen ? "max-h-175 opacity-100 py-4" : "max-h-0 opacity-0 py-0"}`}>
+        <div className="px-[8%] space-y-3">
+          {navLinks.map((link) => (
+              <div key={link.label} className="border border-gray-700/50 rounded-lg overflow-hidden">
+              {link.dropdown ? (
+                <>
+                  <button onClick={() => toggleDropdown(link.label)} className="w-full flex items-center justify-between px-4 py-3 text-left text-(--text) font-medium hover:text-(--prim) transition">
+                    {link.label}
+                    <i className={`ri-arrow-down-s-line transition-transform duration-300 ${openDropdowns[link.label] ? "rotate-180" : ""}`}></i>
+                  </button>
+                  <div className={`pl-6 pr-4 bg-gray-800/10 border-t border-gray-700/40 transition-all duration-500 ${openDropdowns[link.label] ? "max-h-75 opacity-100 py-2" : "max-h-0 opacity-0 py-0"}`}>
+                    {link.dropdown.map((item) => (
+                      <Link 
+                        key={item.label} 
+                        href={item.href} 
+                        className="block py-2 font-semibold hover:text-(--prim) transition border-b border-gray-700/60" 
+                        onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+                      >
+                        {item.label}
+                      </Link>
+                    ))}
+                  </div>
+                </>
+                ) : (
+                  <Link
+                    href={link.href}
+                    className="block px-4 py-3 text-(--text) hover:text-(--prim) transition font-medium"
+                    onClick={() => setMobileMenuOpen(!mobileMenuOpen)}
+                  >
+                    {link.label}
+                  </Link>
+                )}
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
   )
 }
